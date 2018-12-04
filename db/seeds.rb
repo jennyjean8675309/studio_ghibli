d1a3084ff22de8c9d9b921dae5da99b9a693a389
Solee Lee <carolinesoleelee@gmail.com>
2018-12-04 10:58:28 -0500

    created placeholders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Movie.create(name: '')
+Character.create(name: '', image_url: '', likes: '', description: '', movie_id: '')
