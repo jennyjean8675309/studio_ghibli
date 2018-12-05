c53624e8a3bae416367766956b68340f56e06593
jennyjean <jennyjean8675309@yahoo.com>
2018-12-05 10:55:48 -0500

    Added a route for characters#destroy and a destroy action in characters controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :movies, to: 'movies#index'
-      resources :characters, only: [:index, :show, :create]
+      resources :characters, only: [:index, :show, :create, :destroy]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
