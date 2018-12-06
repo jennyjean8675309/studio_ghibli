4fdd2eba2e124888e2e83dc86c25442fb483297c
Solee Lee <carolinesoleelee@gmail.com>
2018-12-06 14:15:01 -0500

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Movie.destroy_all
+Character.destroy_all
+Comment.destroy_all
 #Movies
 Movie.create(name: 'Spirited Away')
 Movie.create(name: 'Nausicaä')
